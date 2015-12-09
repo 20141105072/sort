f2ccd391968dc79a2fa81f91139459b0f0b9a8ca
20141105072 <2196690899@qq.com>
2015-12-09 19:43:02 +0800

    sort
    利用循环定义五个整型数，利用两个循环和if语句，将定义的五个数字从小到大排序，最后将数组a释放。

---
+++ main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  sort
+//
+//  Created by 20141105072 on 15/12/9.
+//  Copyright © 2015年 20141105072. All rights reserved.
+//
+
+#include <iostream>
+#define N 5
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a[N];
+    int i,j,temp;
+    for(i=0;i<N;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<N;i++)
+        printf("%d\n",a[i]);
+    free(a);
+    return 0;
+}
